8b659cc2c767d81507168b650344ef6e149225e3
Eduard Konovka <ed098ua@gmail.com>
2023-06-02 18:14:58 +0300

    Fix .get_rect()

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
-player_rect = player.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+# player_rect = player.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+player_rect = player.get_rect()
 player_speed = [1, 1]
 
 playing = True
@@ -30,16 +31,20 @@ while playing:
     main_display.fill(COLOR_BLACK)
 
     if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
-
-    if player_rect.right >= WIDTH:
-        player_speed[0] = -player_speed[0]
+        # player_speed[1] = -player_speed[1]
+        player_speed = [1, -1]
 
     if player_rect.top <= 0:
-        player_speed[1] = -player_speed[1]
+        # player_speed[1] = -player_speed[1]
+        player_speed = [-1, 1]
+
+    if player_rect.right >= WIDTH:
+        # player_speed[0] = -player_speed[0]
+        player_speed = [-1, -1]
 
     if player_rect.left <= 0:
-        player_speed[0] = -player_speed[0]
+        # player_speed[0] = -player_speed[0]
+        player_speed = [1, 1]
 
     main_display.blit(player, player_rect)
 
